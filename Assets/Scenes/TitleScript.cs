a8a79e79b8675cbde44431d15b7e381fb95fd66b
MiyawakiKosei <shine.star0221@icloud.com>
2025-07-30 11:12:31 +0900

    Title画面の編集、マウスの左ボタンをクリックするとGame画面に飛ぶようにしました

--- Assets/Scenes/TitleScript.cs
+++ Assets/Scenes/TitleScript.cs
@@ -8,17 +8,15 @@ public class TitleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("ChangeScene", 3.0f); // 3.0秒後にシーンを変更
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void ChangeScene()
-    {
-        SceneManager.LoadScene("SampleScene"); // "GameScene" という名前のシーンに変更
+        if(Input.GetMouseButtonDown(0)) // マウスの左ボタンが押されたとき
+        {
+            SceneManager.LoadScene("SampleScene"); // シーンを変更するメソッドを呼び出す
+        }
     }
 }
